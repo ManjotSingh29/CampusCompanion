6d9bbaeda6695c71f74993c80717602da33929ce
anoman6ix <heyitsnoman@gmail.com>
2026-02-03 16:29:08 -0500

    Study Tip changed

--- src/app/features/StudyTipFeature.java
+++ src/app/features/StudyTipFeature.java
@@ -4,10 +4,10 @@ public class StudyTipFeature {
     public static String run() {
         return "Study Tip\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
-             + "Study Style: YOUR STUDY STYLE HERE\n"
+             + "Student Name: Abdullah Al Noman\n"
+             + "Study Style: Not procrastinate\n"
              + "Tip:\n"
-             + "Replace this text with your own study tip.\n"
+             + "focus.\n"
              + "------------------------------\n";
     }
 }
